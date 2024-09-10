cfa0ef359b007f30b7012eae1512667f7ddd693b
Salman Shah <smsalmanshah@gmail.com>
2024-09-10 13:39:00 +0500

    chore: add fonts

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+        fontFamily: {
+          lato: ['Lato', 'sans-serif'],
+          montserrat: ['Montserrat', 'sans-serif'],
+        },
+  
+    },
+  },
+  plugins: [],
+}
