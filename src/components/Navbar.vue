bbff2551d869b26a6ff70241bbc124fa0494298e
Salman Shah <smsalmanshah@gmail.com>
2024-09-18 21:30:40 +0500

    feat:added mobile navbar responsive behaviour

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,31 +1,42 @@
 <script setup>
-import {ref} from 'vue'
-import NavbarIconOpen from '@images/NavbarIconOpen.png'
-import NavbarIconClose from '@images/NavbarIconClose.png'
+import {ref,onMounted, onBeforeUnmount} from 'vue'
+import NavbarIconOpen from '@images/NavbarIconOpen.svg'
+import NavbarIconClose from '@images/NavbarIconClose.svg'
 import Button from '@components/Button.vue';
 
 const isMenuActive = ref(false)
-
 
 function toggleIcon(event){
     console.log("buttonClicked")
     isMenuActive.value = !isMenuActive.value;
     console.log (isMenuActive.value);
-    
 }
+
+function handleResize(){
+    if (document.documentElement.clientWidth > 786){
+    
+    isMenuActive.value=true;
+
+    }
+}
+onMounted(()=>{
+    window.addEventListener('resize',handleResize)
+})
+
 
 </script>
 
 
 <template>
 
-<div class="bg-secondary  flex justify-between  items-center p-4 h-20">
+<div class="bg-secondary  flex justify-between  items-center py-4 h-20">
 
     <a href="#">
         <img src="./../assets/images/Logo.svg" alt="Al Kahaf Logo" srcset=""   class="h-10 ">
     </a>
 
-    <div class=" bg-red-800 text-xs font-bold  font-montserrat text-primary sm:text-l flex flex-col  self-start  sm:self-center ">
+    <div :class="[{ 'h-screen': isMenuActive },
+     'bg-secondary sm:h-fit text-xs font-bold  font-montserrat text-primary sm:text-l flex flex-col  self-start  sm:self-center' ]">
               
             <button @click="toggleIcon" class="sm:hidden  self-center ">
                 
@@ -33,7 +44,8 @@ function toggleIcon(event){
                 :alt="isMenuActive?'Close Menu Icon':'Open Menu Icon'" 
                 class="h-8 ">
             </button>
-            <ul  class="flex flex-col  items-start p-2 py-5 gap-4 sm:items-center sm:flex-row text-xs " >
+            <ul  :class="[{ 'hidden': !isMenuActive },
+    'flex flex-col  items-start px-6 pt-6 gap-4 sm:items-center sm:flex-row text-xs']" >
                 <li class="  "><a href="#">Home</a></li>
                 <li class=" "><a href="#">About Us</a></li>
                 <li class=" "><a href="#"></a>Contact Us</li>
@@ -48,3 +60,8 @@ function toggleIcon(event){
 
 
 </template>
+
+<style scoped>
+
+
+</style>
